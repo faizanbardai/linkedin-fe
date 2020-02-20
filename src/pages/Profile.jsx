3fa3dc97f5775b414eb00f84b5e2278bcb4ffbe3
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-02-20 19:04:37 +0100

    added loading to profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -14,62 +14,66 @@ const mapStateToProps = state => {
 const Profile = props => {
   const [show, setShow] = useState(false);
   if (props.token || localStorage.getItem("token")) {
-    const {
-      firstName,
-      lastName,
-      imageProfile,
-      bio,
-      area,
-      title,
-      experiences
-    } = props.user;
-    return (
-      <section className="container my-3">
-        <div className="row d-flex justify-content-center">
-          <div className="col-sm-12 col-md-3 mb-3">
-            <Image
-              src={imageProfile}
-              className="p-4"
-              roundedCircle
-              style={{ width: "100%" }}
-            />
-            <Card>
-              <Card.Header>{title}</Card.Header>
-              <Card.Body>
-                <Card.Title>
-                  {firstName} {lastName}
-                </Card.Title>
-                <Card.Text>{bio}</Card.Text>
-              </Card.Body>
-              <Card.Footer className="text-muted">{area}</Card.Footer>
-            </Card>
-            <Button
-              onClick={() => setShow(true)}
-              variant="outline-primary"
-              block
-              className="my-2 rounded-pill"
-            >
-              Edit Profile
-            </Button>
-
-            {props.user.username && (
-              <UpdateProfile show={show} setShow={setShow} />
-            )}
+    if (props.user) {
+      const {
+        firstName,
+        lastName,
+        imageProfile,
+        bio,
+        area,
+        title,
+        experiences
+      } = props.user;
+      return (
+        <section className="container my-3">
+          <div className="row d-flex justify-content-center">
+            <div className="col-sm-12 col-md-3 mb-3">
+              <Image
+                src={imageProfile}
+                className="p-4"
+                roundedCircle
+                style={{ width: "100%" }}
+              />
+              <Card>
+                <Card.Header>{title}</Card.Header>
+                <Card.Body>
+                  <Card.Title>
+                    {firstName} {lastName}
+                  </Card.Title>
+                  <Card.Text>{bio}</Card.Text>
+                </Card.Body>
+                <Card.Footer className="text-muted">{area}</Card.Footer>
+              </Card>
+              <Button
+                onClick={() => setShow(true)}
+                variant="outline-primary"
+                block
+                className="my-2 rounded-pill"
+              >
+                Edit Profile
+              </Button>
+              {props.user.username && (
+                <UpdateProfile show={show} setShow={setShow} />
+              )}
+            </div>
+            <div className="col-sm-12 col-md-9">
+              <Card>
+                <Card.Body>
+                  <Card.Title>Experience</Card.Title>
+                  {experiences &&
+                    experiences.map(experience => (
+                      <Experience
+                        key={experience._id}
+                        experience={experience}
+                      />
+                    ))}
+                </Card.Body>
+              </Card>
+            </div>
           </div>
-          <div className="col-sm-12 col-md-9">
-            <Card>
-              <Card.Body>
-                <Card.Title>Experience</Card.Title>
-                {experiences &&
-                  experiences.map(experience => (
-                    <Experience key={experience._id} experience={experience} />
-                  ))}
-              </Card.Body>
-            </Card>
-          </div>
-        </div>
-      </section>
-    );
+        </section>
+      );
+    } else return <div>Loading...</div>;
   }
   return <Redirect push to="/login" />;
 };
